d56459a762b2dfa5d51b737fa62ee0036b61dbf6
Will <will7989@hotmail.com>
2019-08-07 11:10:27 +0100

    change docker-compose port. Added test for request directory trailing slash

---
+++ main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+
+func TestRequestPathTrailingSlash(t *testing.T) {
+
+	testCases := []struct {
+		Name           string
+		Input          string
+		ExpectedOutput string
+	}{
+		{
+			Name:           "Has trailing slash, returned unchanged",
+			Input:          "request/",
+			ExpectedOutput: "request/",
+		},
+		{
+			Name:           "Does not have trailing slash, returned with slash",
+			Input:          "request",
+			ExpectedOutput: "request/",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.Name, func(t *testing.T) {
+			makeSureRequestDirectoryHasTrailingSlash(&test.Input)
+
+			if test.Input != test.ExpectedOutput {
+				t.Errorf("got %v want %v", test.Input, test.ExpectedOutput)
+			}
+		})
+	}
+
+}
